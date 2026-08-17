d269aa00f207d2f40aee6c1b2725f2cf4a534ef3
Oussama ELAYOUCHI <90898909+Oelayouchi@users.noreply.github.com>
2026-08-17 16:19:36 +0200

    Charge la mise en page finale des projets

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import './alternance.css';
 import './theme.css';
 import './education-interests.css';
 import './projects-media.css';
+import './projects-layout-final.css';
 import './contact.css';
 import './companies-strip.css';
 import './navigation-cleanup.css';
